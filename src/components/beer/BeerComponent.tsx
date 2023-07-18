6fdbd0206019cdf61c325a56eb5995cb39f3312a
Anthony Limon <pro.anthony.limon@gmail.com>
2023-07-18 11:08:52 +0200

    Changed design of BeerComponent

--- src/components/beer/BeerComponent.tsx
+++ src/components/beer/BeerComponent.tsx
@@ -1,9 +1,18 @@
 import {TBeer} from "types/Beer.type";
-import {Card, CardActionArea, CardContent, styled, Typography,} from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
 import {grey} from "@mui/material/colors";
 import useThemeStore from "store/useTheme.store";
 import useBeersStore from "store/useBeers.store";
 import {useNavigate} from "react-router-dom";
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 /**
  * BeerCard
@@ -59,17 +68,43 @@ const BeerComponent = ({ beer }: TBeerComponentProps) => {
       sx={{ backgroundColor: isSelected ? backgroundColor : "inherit" }}
     >
       <CardActionArea
-        sx={{ width: "100%", height: "100%" }}
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+          p: 2,
+        }}
         onClick={handleClick}
         onContextMenu={handleContextMenu}
       >
-        <CardContent sx={{ width: "100%", height: "100%", overflow: "scroll" }}>
-          <Typography variant={"h5"} color={"primary"} gutterBottom>
-            {beer.name}
-          </Typography>
-          <Typography variant={"body2"} overflow={"scroll"}>
-            {beer.description}
-          </Typography>
+        <CardMedia
+          component={"img"}
+          src={beer?.image_url}
+          sx={{ width: "auto", height: "100%", objectFit: "fill", m: "auto" }}
+        />
+        <CardContent sx={{ width: "60%", height: "100%" }}>
+          <Stack spacing={3}>
+            <Typography variant={"h5"} color={"primary"} gutterBottom>
+              {beer.name}
+            </Typography>
+            <Grid2 container>
+              <Grid2 xs={12} display={"flex"} alignItems={"center"}>
+                <Typography variant={"h6"} color={"primary"} mr={2}>
+                  Volume :
+                </Typography>
+                <Typography variant={"body2"}>
+                  {beer.volume.value} {beer.volume.unit}
+                </Typography>
+              </Grid2>
+              <Grid2 xs={12} display={"flex"} alignItems={"center"}>
+                <Typography variant={"h6"} color={"primary"} mr={2}>
+                  ABV :
+                </Typography>
+                <Typography variant={"body2"}>{beer.abv} %</Typography>
+              </Grid2>
+            </Grid2>
+          </Stack>
         </CardContent>
       </CardActionArea>
     </BeerCard>
